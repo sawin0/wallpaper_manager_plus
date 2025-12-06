b2ef58dd2e74e8253b3112a5bee2966ca9c9cec6
Sabin Ranabhat <binranabhat@gmail.com>
2025-12-06 19:01:33 +0545

    fix: add file on plugin integration test

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -6,9 +6,10 @@
 // For more information about Flutter integration tests, please see
 // https://docs.flutter.dev/cookbook/testing/integration/introduction
 
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:wallpaper_manager_plus/wallpaper_manager_plus.dart';
 
 void main() {
@@ -16,6 +17,16 @@ void main() {
 
   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
     final WallpaperManagerPlus plugin = WallpaperManagerPlus();
-    await plugin.setWallpaper(0, 0);
+
+    // Create a temporary placeholder file and write a single byte.
+    final tmpDir = await Directory.systemTemp.createTemp('wallpaper_test');
+    final file = File('${tmpDir.path}/wallpaper.gif');
+    await file.writeAsBytes([0]);
+
+    // Pass the File to setWallpaper (location remains an int).
+    await plugin.setWallpaper(file, 0);
+
+    // Clean up the temporary directory.
+    await tmpDir.delete(recursive: true);
   });
 }
